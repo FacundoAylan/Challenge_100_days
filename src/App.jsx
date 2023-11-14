c155a8d0e5c3023a15b3dea3683e1961fee812a6
Facundo <facundogsb@gmail.com>
2023-11-13 22:16:25 -0300

    subiendo el primer challenge

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Desafio_1 from './component/desafio_01/01_desafio';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <Desafio_1/>
+    </>
+  )
+}
+
+export default App
---
+++ src/component/desafio_01/01_desafio.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './01_index.css'
+
+function Desafio_1(){
+  return(
+    <div className='container_1'>
+      <div className='center'>
+        <div className='container_number'>
+          <span className='one_one'></span>
+          <span className='one_two'></span>
+          <span className='zero_one'></span>
+          <span className='zero_two'></span>
+        </div>
+        <span className='days'>DAYS</span>
+        <span className='css'>CSS CHALLANGE</span>
+      </div>
+    </div>
+  )
+}
+
+export default Desafio_1;
